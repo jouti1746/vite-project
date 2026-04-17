4c823d5d9bd33b8ce22151de63c94d0b3e553d96
Umma Habiba Jouti <jouti.habibi1746@gmail.com>
2026-04-17 17:32:12 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-7xl text-amber-500'>Vite + React</h1>
+
+     
+      
+     
+    </>
+  )
+}
+
+export default App
